c586529338fafd6de52f21c2fb8201315ca111c0
peterjjonas <peterjjonas@tutanota.com>
2021-01-14 16:31:21 +0000

    Clear the code

--- server.js
+++ server.js
@@ -1,105 +1,101 @@
 const express = require('express');
 const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
+const knex = require('knex');
+
+const db = knex({
+  client: 'pg',
+  connection: {
+    host : '127.0.0.1',
+    user : 'peterjjonas',
+    password : '',
+    database : 'smart-brain'
+  }
+});
 
 const app = express();
 const port = 3003;
 
-// app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 app.use(cors());
 
-const database = {
-  users: [
-    {
-      id: '123',
-      name: 'Ross',
-      email: 'ross@typo.com',
-      password: 'sandiabla',
-      entries: 0,
-      joined: new Date()
-    },
-    {
-      id: '124',
-      name: 'Rosa',
-      email: 'rosa@typo.com',
-      password: 'sandiablo',
-      entries: 0,
-      joined: new Date()
-    }
-  ],
-  login: [
-    {
-      id: '345',
-      hash: '',
-      email: 'rossssss@typo.com'
-    }
-  ]
-}
-
 app.get('/', (req, res) => {
-  res.send(database.users);
-})
+  res.send('<h1 style="text-align: center; padding: 10px;">Server is running.<br>OK</h1>');
+});
+
 
 app.post('/signin', (req, res) => {
-  if (req.body.email === database.users[0].email &&
-    req.body.password === database.users[0].password) {
-      console.log(database.users[0]);
-    res.json(database.users[0]);
-  } else {
-    res.status(400).json('error logging in');
-  }
+  db.select('email', 'hash').from('login')
+    .where('email', '=', req.body.email)
+    .then(data => {
+      const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
+      if (isValid) {
+        return db.select('*').from('users')
+        .where('email', '=', req.body.email)
+        .then(user => {
+          res.json(user[0])
+        })
+        .catch(err => res.status(400).json('unable to get user'))
+      } else {
+        res.status(400).json('wrong credentials')
+      }
+    })
+    .catch(err => res.status(400).json('wron credentials'))
 })
 
 app.post('/register', (req, res) => {
   const { email, name, password } = req.body;
-  database.users.push({
-    id: '125',
-    name: name,
-    email: email,
-    entries: 0,
-    joined: new Date()
-  })
-  res.json(database.users[database.users.length-1]);
+  const hash = bcrypt.hashSync(password);
+    db.transaction(trx => {
+      trx.insert({
+        hash: hash,
+        email: email
+      })
+      .into('login')
+      .returning('email')
+      .then(loginEmail => {
+        return trx('users')
+          .returning('*')
+          .insert({
+            email: loginEmail[0],
+            name: name,
+            joined: new Date()
+          })
+          .then(user => {
+            res.json(user[0]);
+          })
+      })
+      .then(trx.commit)
+      .catch(trx.rollback)
+    })
+    .catch(err => res.status(400).json('unable to register'))
 })
 
 app.get('/profile/:id', (req, res) => {
   const { id } = req.params;
-  let found = false;
-  database.users.forEach(user => {
-    if (user.id === id) {
-      found = true;
-      return res.json(user);
+  db.select('*').from('users').where({
+    id: id
+  })
+  .then(user => {
+    if (user.length) {
+      res.json(user[0]);
+    } else {
+      res.status(404).json('not found')
     }
   })
-  if (!found) {
-    res.status(400).json('no such user');
-  }
+  .catch(err => res.status(400).json('error getting users'))
 })
 
 app.put('/image', (req, res) => {
   const { id } = req.body;
-  let found = false;
-  database.users.forEach(user => {
-    if (user.id === id) {
-      found = true;
-      user.entries++
-      return res.json(user.entries);
-    }
+  db('users').where('id', '=', id)
+  .increment('entries', 1)
+  .returning('entries')
+  .then(entries => {
+    res.json(entries[0]);
   })
-  if (!found) {
-    res.status(404).json('no such user');
-  }
+  .catch(err => res.status(400).json('unable to get entries'))
 })
-
-// bcrypt.hash(password, null, null, function(err, hash) {
-//     console.log(hash);
-// });
-//
-
-
-
-
 
 app.listen(port, () => {
   console.log('app is runing on port', port);
