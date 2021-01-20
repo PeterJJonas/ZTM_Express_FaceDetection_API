551518109f6999daaf007f039d85b516271bb758
peterjjonas <peterjjonas@tutanota.com>
2021-01-20 15:43:07 +0000

    add provess.env to PORT

--- server.js
+++ server.js
@@ -31,4 +31,4 @@ app.get('/profile/:id', (req, res) =>{profile.handleProfileGet(req, res, db)})
 app.put('/image', (req, res) => {image.handleImage(req, res, db)})
 app.post('/imageurl', (req, res) => {image.handleApiCall(req, res)})
 
-app.listen(process.env.PORT || 3000, () => {console.log(`app is runing on port${PORT}`) })
+app.listen(process.env.PORT || 3000, () => {console.log(`app is runing on port${process.env.PORT}`) })
